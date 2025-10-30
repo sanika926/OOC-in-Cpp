682ffbb896acb59bf28d66b121c680d8379995ce
Sanika Mane <msanika487@gmail.com>
2025-10-30 19:50:07 +0530

    Added Assignment 08_program1 file

--- Assignments/Assignment 08_program1.cpp
+++ Assignments/Assignment 08_program1.cpp
@@ -22,8 +22,17 @@ public:
 };
 
 int main() {
+
+    
+    // ---- My Information ---- 
+    cout << "Name: Sanika Anil Mane\n"; 
+    cout << "Roll No: 96\n"; 
+    cout << "Division: B\n"; 
+    cout << "Class: SY CSE\n"; 
+    cout << "---------------------------\n";
+    
     Distance d1(8, 9);
-    -d1;
+    --d1;
     d1.display();
     return 0;
 }
@@ -31,4 +40,10 @@ int main() {
 
 //OUTPUT//
 
+/*Name: Sanika Anil Mane
+Roll No: 96
+Division: B
+Class: SY CSE
+---------------------------
 Feet & Inches (Decrement): 7 8
+*/
