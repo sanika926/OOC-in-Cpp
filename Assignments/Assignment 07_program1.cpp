ee561368e5f2f1d4ab68ea3b2e40eb59d59a9e74
Sanika Mane <msanika487@gmail.com>
2025-10-30 19:42:08 +0530

    Added Assignment 07_program1 file

--- Assignments/Assignment 07_program1.cpp
+++ Assignments/Assignment 07_program1.cpp
@@ -28,6 +28,14 @@ public:
 };
 
 int main() {
+    
+    // ---- My Information ---- 
+    cout << "Name: Sanika Anil Mane\n"; 
+    cout << "Roll No: 96\n"; 
+    cout << "Division: B\n"; 
+    cout << "Class: SY CSE\n"; 
+    cout << "---------------------------\n";
+    
     Car obj;   // Creating object of derived class
     return 0;
 }
@@ -35,7 +43,13 @@ int main() {
 
 //OUTPUT//
 
+/*Name: Sanika Anil Mane
+Roll No: 96
+Division: B
+Class: SY CSE
+---------------------------
+
 This is a Vehicle
 This is a Four Wheeler
 This Four Wheeler Vehicle is a Car
-
+*/
